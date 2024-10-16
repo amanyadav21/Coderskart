2268e0ccfdebd4f39f64ae82094c904961b755af
amanyadav21 <amankumaryadav1029@gmail.com>
2024-10-17 02:23:48 +0530

    upgrade

--- Project root/api/frontendapi.js
+++ Project root/api/frontendapi.js
@@ -3,10 +3,7 @@ function fetchData() {
     return [
       { imageUrl: "/Project root/Images/frontend/color/colorhunt.png", text: "Color Hunt is a free collection of curated color palettes.", url: "https://colorhunt.co" },
       { imageUrl: "/Project root/Images/frontend/color/adobecolor.png", text: "Adobe Color is a tool for creating and exploring color palettes.", url: "https://color.adobe.com" },
-      { imageUrl: "/Project root/Images/frontend/color/coolors.png", text: "Colors is an online platform for exploring and generating color palettes.", url: "https://coolors.co" },
-      { imageUrl: "/Project root/Images/frontend/coolors.png", text: "Colors is an online platform for exploring and generating color palettes.", url: "https://coolors.co" },
-      { imageUrl: "/Project root/Images/frontend/coolors.png", text: "Colors is an online platform for exploring and generating color palettes.", url: "https://coolors.co" },
-      { imageUrl: "/Project root/Images/frontend/coolors.png", text: "Colors is an online platform for exploring and generating color palettes.", url: "https://coolors.co" }
+      { imageUrl: "/Project root/Images/frontend/color/coolors.png", text: "Colors is an online platform for exploring and generating color palettes.", url: "https://coolors.co" }
     ];
   }
 
