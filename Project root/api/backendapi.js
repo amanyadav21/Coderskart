defc93c2bb29ba4c3f08df37896c987269df1ebe
amanyadav21 <amankumaryadav1029@gmail.com>
2024-10-18 21:56:13 +0530

    upgrade

--- Project root/api/backendapi.js
+++ Project root/api/backendapi.js
@@ -1,20 +1,40 @@
 // Simulating an API call to fetch data
 function fetchData() {
   return [
-    { h: "Color Hunt",
+    { h: "Aws",
       imageUrl: "/Project root/Images/Backend/cloud platforms/aws.png", 
-      text: "Color Hunt is a free collection of curated color palettes.", 
-      url: "https://colorhunt.co" 
+      text: "AWS is Amazon's cloud platform offering services like computing power, storage, and databases, supporting scalable and flexible cloud solutions.", 
+      url: "https://aws.amazon.com/?nc2=h_lg" 
     },
-    { h: "Adobe Color",
-      imageUrl: "/Project root/Images/frontend/color/adobecolor.png", 
-      text: "Adobe Color is a tool for creating and exploring color palettes.", 
-      url: "https://color.adobe.com" 
+    { h: "Firebase",
+      imageUrl: "/Project root/Images/Backend/cloud platforms/firebase.png", 
+      text: "Firebase, by Google, provides tools like databases and analytics to help developers build and grow web and mobile apps faster", 
+      url: "https://firebase.google.com/" 
     },
-    { h: "Colors",
-      imageUrl: "/Project root/Images/frontend/color/coolors.png", 
-      text: "Colors is an online platform for exploring and generating color palettes.", 
+    { h: "MongoDB Alts",
+      imageUrl: "/Project root/Images/Backend/cloud platforms/mangodb alters.png", 
+      text: "MongoDB Atlas is a fully managed NoSQL database service that allows developers to run MongoDB databases on major cloud platforms, offering scalability and security.", 
+      url: "https://www.mongodb.com/products/platform/atlas-database" 
+    },
+    { h: "Azure",
+      imageUrl: "/Project root/Images/Backend/cloud platforms/azure.png", 
+      text: "Azure is Microsoft's cloud platform providing a range of services like AI, machine learning, and storage for scalable application development..", 
+      url: "https://azure.microsoft.com/en-in" 
+    },
+    { h: "Digital ocean",
+      imageUrl: "/Project root/Images/Backend/cloud platforms/digitalocean.png", 
+      text: "DigitalOcean offers virtual servers and scalable cloud solutions, focused on simplicity for developers and small businesses..", 
+      url: "https://www.digitalocean.com/" 
+    },
+    { h: "Google Cloud",
+      imageUrl: "/Project root/Images/Backend/cloud platforms/google-cloud.png", 
+      text: "Google Cloud offers services like storage, computing, and machine learning, known for speed, reliability, and Google integration..", 
       url: "https://coolors.co" 
+    },
+    { h: "Heroku",
+      imageUrl: "/Project root/Images/Backend/cloud platforms/heroku (1).png", 
+      text: "Heroku is a cloud platform that lets developers build, run, and deploy applications easily, known for its simplicity and multi-language support.", 
+      url: "https://www.heroku.com/" 
     }
   ];
 }
