1ce8f6bced6ffc5ba6b00aa44bd6541c2b493cea
amanyadav21 <amankumaryadav1029@gmail.com>
2024-10-24 01:37:53 +0530

    non coding based

---
+++ Project root/api/noncodingapi.js
@@ -0,0 +1,35 @@
+// Simulating an API call to fetch data cloud platform
+function fetchData() {
+    return [
+      { h: "Webflow",
+        imageUrl: "/Project root/Images/Non Coding/webflow-new2506.logowik.com.webp", 
+        text: "Ideal for solving coding problems, learning new programming concepts, generating quick code examples, and automating simple tasks like generating documentation or writing test cases.", 
+        url: "https://openai.com/chatgpt" 
+      },
+  
+      { h: "Wix Stdio",
+        imageUrl: "/Project root/Images/Non Coding/Wix logo white BG.webp", 
+        text: "A powerful tool for automating network operations, offering AI-driven network analysis and compliance checks.", 
+        url: "https://www.blackbox.ai/" 
+      },
+
+      { h: "Elementor(WordPress)",
+        imageUrl: "/Project root/Images/Non Coding/elementor.svg", 
+        text: "A powerful tool for automating network operations, offering AI-driven network analysis and compliance checks.", 
+        url: "https://www.blackbox.ai/" 
+      },
+
+      { h: "Carrd",
+        imageUrl: "/Project root/Images/Non Coding/carrd.image.jpg", 
+        text: "A powerful tool for automating network operations, offering AI-driven network analysis and compliance checks.", 
+        url: "https://www.blackbox.ai/" 
+      },
+
+      { h: "SquareSpace",
+        imageUrl: "/Project root/Images/Non Coding/squarespace-logo-horizontal-black.jpg", 
+        text: "A powerful tool for automating network operations, offering AI-driven network analysis and compliance checks.", 
+        url: "https://www.blackbox.ai/" 
+      }
+    ];
+  }
+  
