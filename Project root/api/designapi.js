551f5c0a592e9e0901af612549ca7b32763d9969
amanyadav21 <amankumaryadav1029@gmail.com>
2024-10-26 23:08:46 +0530

    design

---
+++ Project root/api/designapi.js
@@ -0,0 +1,70 @@
+// Simulating an API call to fetch data cloud platform
+function fetchData() {
+    return [
+      { h: "Webflow",
+        imageUrl: "/Project root/Images/Non Coding/Website/Webflow.jpg", 
+        text: "Webflow is more advanced than most drag-and-drop builders and is targeted at developers and designers who want more control over the design without diving into the code. It allows you to create responsive websites visually and export the clean HTML/CSS code if needed.", 
+        url: "https://www.webflow.com" 
+      },
+  
+      { h: "Wix Studio",
+        imageUrl: "/Project root/Images/Non Coding/Website/WIx.jpg", 
+        text: "One of the most popular drag-and-drop website builders. Wix offers a wide range of customizable templates, a flexible drag-and-drop editor, and tons of built-in features, such as SEO tools, mobile optimization, and e-commerce solutions.", 
+        url: "https://www.wix.com/" 
+      },
+
+      { h: "Elementor(WordPress)",
+        imageUrl: "/Project root/Images/Non Coding/Website/Elementor.jpg", 
+        text: "Elementor is one of the most powerful drag-and-drop page builders for WordPress. It allows you to create beautiful pages with a live preview and offers a wide range of design elements, templates, and integration options.", 
+        url: "https://www.elementor.com/" 
+      },
+
+      { h: "Carrd",
+        imageUrl: "/Project root/Images/Non Coding/Website/Carrd.jpg", 
+        text: "Carrd specializes in creating simple, responsive one-page websites. It's perfect for landing pages, portfolios, and personal websites, offering a straightforward drag-and-drop interface.", 
+        url: "https://www.carrd.co/" 
+      },
+
+      { h: "SquareSpace",
+        imageUrl: "/Project root/Images/Non Coding/Website/Squarespace.jpg", 
+        text: " Known for its sleek and professional design templates, Squarespace is great for creating visually stunning websites without any coding. It provides built-in tools for blogging, e-commerce, and marketing.", 
+        url: "https://www.squarespace.com/" 
+      }
+    ];
+  }
+
+
+// Simulating an API call to fetch data cloud platform
+function fetchMobileData() {
+    return [
+      { h: "Flutter Flow",
+        imageUrl: "/Project root/Images/Non Coding/Mobile/Flutterflow.jpg", 
+        text: "FlutterFlow allows developers to build high-quality mobile applications using Google’s Flutter framework. It supports real-time previews, Firebase integration, and easy export of Flutter code.", 
+        url: "https://www.flutterflow.io/" 
+      },
+  
+      { h: "AppSheet",
+        imageUrl: "/Project root/Images/Non Coding/Mobile/AppSheet.jpg", 
+        text: "AppSheet, now part of Google Cloud, is a powerful platform for building mobile and web apps based on data sources like Google Sheets or Excel. It's perfect for businesses looking to automate workflows.", 
+        url: "https://www.appsheet.com/" 
+      },
+
+      { h: "Bubble",
+        imageUrl: "/Project root/Images/Non Coding/Mobile/Bubble.jpg", 
+        text: "Bubble is a robust no-code platform that allows users to build complex web applications and mobile apps using a drag-and-drop editor. It’s ideal for building both web apps and mobile-friendly applications.", 
+        url: "https://www.bubble.io/" 
+      },
+
+      { h: "Thinkable",
+        imageUrl: "/Project root/Images/Non Coding/Mobile/Thinkable.jpg", 
+        text: "Thunkable is a no-code mobile app builder that supports native mobile app development for both iOS and Android. It has an intuitive drag-and-drop interface and offers a wide range of features, including third-party integrations.", 
+        url: "https://www.thunkable.com/" 
+      },
+
+      { h: "Adolo",
+        imageUrl: "/Project root/Images/Non Coding/Mobile/Adolo.jpg", 
+        text: "Adalo is a no-code platform that enables users to build fully functional mobile and web applications using a drag-and-drop interface. You can create native apps for both iOS and Android.", 
+        url: "https://www.adalo.com/" 
+      }
+    ];
+  }
