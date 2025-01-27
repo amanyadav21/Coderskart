7781d277173e5d56d4ad5d168d5749bcaaf8d619
amanyadav21 <amankumaryadav1029@gmail.com>
2025-01-27 19:38:57 +0530

    Upgrade designer section and add more benifical word which is best way to describe every product

--- Project root/api/designapi.js
+++ Project root/api/designapi.js
@@ -3,25 +3,25 @@ function fetchData() {
     return [
       { h: "Figma",
         imageUrl: "/Project root/Images/design/design tool/Figma.jpg", 
-        text: " Figma is a collaborative, cloud-based design platform popular for UI/UX design and prototyping. Known for its real-time collaboration, Figma allows multiple designers to work simultaneously. Key features include vector editing, auto-layout for responsive design, interactive prototyping, and a vast plugin library", 
+        text: "Figma is a browser-based design tool that allows users to collaborate on user interfaces UI. It's used to create prototypes, wireframes, mockups, and more for websites, mobile apps, and other digital products", 
         url: "https://www.figma.com" 
       },
   
       { h: "Sketch",
         imageUrl: "/Project root/Images/design/design tool/skatch.jpg", 
-        text: "Sketch is a vector-based design tool widely used for creating clean, high-quality UI and icon designs. It’s known for symbols, reusable components, and an intuitive interface. Sketch’s powerful plugin ecosystem and export options make it a top choice for design professionals seeking precision and flexibility..", 
+        text: "Sketch is a vector-based design tool used to create user interfaces (UI) and user experiences (UX) for mobile apps and websites.", 
         url: "https://www.sketch.com/" 
       },
 
       { h: "Adobe XD",
         imageUrl: "/Project root/Images/design/design tool/adobe XD.jpg", 
-        text: "Adobe XD offers a seamless experience for UI/UX design with robust tools for wireframing, prototyping, and animation. Integrated with the Adobe Creative Cloud, XD allows designers to leverage other Adobe assets. Features include voice prototyping, repeat grid, and support for both vector and raster formats, making it ideal for creating dynamic user experiences.", 
+        text: "Adobe XD is a vector design tool that allows users to create interactive prototypes for web and mobile applications. It's used by user experience (UX) and interaction designers to create wireframes, screen designs, and prototypes.", 
         url: "https://adobexdplatform.com/" 
       },
 
       { h: "Canva",
         imageUrl: "/Project root/Images/design/design tool/Canva.jpg", 
-        text: "Canva is an accessible design tool for creating a wide range of visuals, from social media graphics to quick mockups. Its drag-and-drop editor and customizable templates make it beginner-friendly, while collaborative options and a vast library of icons, images, and fonts support a variety of design projects.", 
+        text: "Canva is an online graphic design tool that helps users create visual content. It's used by individuals and businesses to create designs for social media, presentations, websites, and more.", 
         url: "https://www.canva.com/" 
       }
     ];
@@ -33,19 +33,19 @@ function fetchshowcaseData() {
     return [
       { h: "Dribbble",
         imageUrl: "/Project root/Images/design/showcase/dribble.jpg", 
-        text: "Dribbble is a community platform for designers to showcase their work, connect with other creatives, and find inspiration. It features a wide range of design projects, including UI/UX designs, illustrations, and branding work. Users can follow other designers, comment on work, and participate in design challenges, making it a vibrant hub for design enthusiasts.", 
+        text: "Dribbble is a social media platform that allows designers to share their work, find inspiration, and get hired. It's a place for designers to showcase their portfolios, learn from others, and connect with potential employers.", 
         url: "https://dribbble.com/" 
       },
   
       { h: "Behance",
         imageUrl: "/Project root/Images/design/showcase/behance.jpg", 
-        text: "Behance is an online platform for creative professionals to showcase their portfolios across various fields, including graphic design, photography, illustration, and motion graphics. It allows users to discover and appreciate creative work from around the world, making it a powerful tool for networking and collaboration. Behance also integrates with Adobe Creative Cloud, facilitating easy uploads from Adobe software.", 
+        text: "The primary goal of Behance is to create a collection of your work, similar to a portfolio, that highlights what you can do. Free to use, the benefit of Behance versus other social media platforms is that it's specifically for creatives.", 
         url: "https://www.behance.net/" 
       },
 
       { h: "Awwwards",
         imageUrl: "/Project root/Images/design/showcase/awwards.jpg", 
-        text: " Awwwards is an award-winning platform that recognizes and promotes the best web design and development projects globally. It features a curated collection of websites judged by a panel of industry experts based on design, creativity, and innovation. Awwwards serves as both inspiration and a benchmark for web designers and developers looking to create exceptional online experiences.", 
+        text: "Awwwards is a platform recognizing top web design and development projects globally. Experts judge sites based on design, creativity, and innovation, providing inspiration and a benchmark for web professionals.", 
         url: "https://www.awwwards.com/" 
       }
     ];
